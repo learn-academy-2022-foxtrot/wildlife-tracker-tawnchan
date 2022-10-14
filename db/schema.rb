d63d4b9524ed44427bdd4786c0c22b99afdf2b69
tawnchan <tawnchan@gmail.com>
2022-10-14 11:38:32 -0700

    completed Sightings CRUD actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_232344) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_172421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
     t.string "commonName"
     t.string "scientificBinomial"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.integer "animal_id"
+    t.integer "latitude"
+    t.integer "longitude"
+    t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
