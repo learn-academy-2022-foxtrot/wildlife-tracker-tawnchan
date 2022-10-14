79268a4ee3a06292c1f6dfd16dc273e1060fd507
tawnchan <tawnchan@gmail.com>
2022-10-14 09:57:57 -0700

    completed all CRUD actions

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -19,6 +19,25 @@ class AnimalsController < ApplicationController
         end
     end
 
+    def destroy
+        animal = Animal.find(params[:id])
+        if animal.destroy
+            render json: animal
+        else
+            render json: animal.errors
+        end
+    end
+
+    def update
+        animal = Animal.find(params[:id])
+        animal.update(animal_params)
+        if animal.valid?
+            render json: animal
+        else
+            render json: animal.errors
+        end
+    end
+
     private
     def animal_params
     params.require(:animal).permit(:commonName, :scientificBinomial)
