34582d4dc2476a4b6467123c6fc2a9285d9df1c7
tawnchan <tawnchan@gmail.com>
2022-10-14 09:43:14 -0700

    completed crud CREATE functionality and route

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -10,5 +10,17 @@ class AnimalsController < ApplicationController
         render json: animal
     end
 
+    def create
+        animal = Animal.create(animal_params)
+        if animal.valid?
+            render json: animal
+        else
+            render json: animal.errors
+        end
+    end
 
+    private
+    def animal_params
+    params.require(:animal).permit(:commonName, :scientificBinomial)
+    end
 end
